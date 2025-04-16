19171bb692929469d72568b051fb2549dd24431f
deepw98 <143339991+deepw98@users.noreply.github.com>
2025-04-16 10:12:19 +0530

    express practice

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,42 @@
-console.log('Express Tutorial')
+const express = require('express')
+const app = express()
+const {people} = require('./data')
+
+app.use(express.static('./methods-public'))
+app.use(express.json())
+
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people })
+})
+
+app.post('/api/people',(req,res)=>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success:false,msg:'Please provide credentials'})
+    }
+    res.status(201).json({success:true,person:name})
+})
+
+app.put('/api/people/:id',(req,res)=>{
+    const {id} = req.params;
+    const {name} = req.body;
+    
+    const person = people.find((person)=>person.id === Number(id))
+
+    if(!person){
+        return res.status(404).json({success:false,msg:`no person with id:${id}`})
+    }
+    const newPeople = people.map((person)=>{
+        if(person.id === Number(id)){
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({success:true, data:newPeople})
+
+})
+
+
+app.listen(5000,()=>{
+    console.log('Server is listening on port 5000...')
+})
